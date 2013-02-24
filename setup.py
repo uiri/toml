6d1fc458cc3f0cd11ef4b9b553d349bb48fb12da
Uiri <uiri00@gmail.com>
2013-02-24 16:58:44 -0500

    Add stuff for packaging and update gitignore to ignore pyc files

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='toml',
+      version='0.6.0',
+      description="Python Library for Tom's Obvious, Minimal Language",
+      author="Uiri Noyb",
+      author_email="uiri@xqz.ca",
+      url="https://github.com/uiri/toml",
+      py_modules=['toml'],
+      license="License :: OSI Approved :: MIT License",
+)
