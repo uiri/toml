a97baa81df3255367a99b5e47186ed1130ab2829
octycs <println.py@gmail.com>
2015-02-05 19:55:34 +0100

    Fix endless loop if datatype is unknown.

--- toml.py
+++ toml.py
@@ -253,8 +253,7 @@ def loads(s, _dict=dict):
                         datetime.datetime.strptime(pair[-1], "%Y-%m-%dT%H:%M:%SZ")
                         break
                     except ValueError:
-                        i += 1
-                        pair = line.split('=', i)
+                        raise Exception("What's the datatype of '"+pair[-1]+"'?")
             newpair = []
             newpair.append('='.join(pair[:-1]))
             newpair.append(pair[-1])
