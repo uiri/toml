1b5bb9e6d6cc95a41a99763aedc75a71314cea7c
Uiri <uiri00@gmail.com>
2017-10-08 05:00:40 -0400

    Close #91 - add Toml Array Separator Encoder

--- toml/encoder.py
+++ toml/encoder.py
@@ -198,3 +198,31 @@ class TomlEncoder(object):
                 retdict[qsection] = o[section]
         retstr += arraystr
         return (retstr, retdict)
+
+
+class TomlArraySeparatorEncoder(TomlEncoder):
+
+    def __init__(self, _dict=dict, preserve=False, separator=","):
+        super(self.__class__, self).__init__(_dict, preserve)
+        if separator.strip() == "":
+            separator = "," + separator
+        elif separator.strip(' \t\n\r,'):
+            raise ValueError("Invalid separator for arrays")
+        self.separator = separator
+
+    def dump_list(self, v):
+        t = []
+        retval = "["
+        for u in v:
+            t.append(self.dump_value(u))
+        while t != []:
+            s = []
+            for u in t:
+                if isinstance(u, list):
+                    for r in u:
+                        s.append(r)
+                else:
+                    retval += " " + unicode(u) + self.separator
+            t = s
+        retval += "]"
+        return retval
