c22b05c4a2aaa0dd03e07a31f0e346b542545a3d
Uiri <uiri00@gmail.com>
2017-10-08 04:41:21 -0400

    Remove underscores. Put dump_value in the encoder.

--- toml/__init__.py
+++ toml/__init__.py
@@ -6,7 +6,7 @@ Released under the MIT license.
 from toml import encoder
 from toml import decoder
 
-__version__ = "0.9.3"
+__version__ = "0.10.0"
 __spec__ = "0.4.0"
 
 load = decoder.load
--- toml/encoder.py
+++ toml/encoder.py
@@ -44,12 +44,12 @@ def dumps(o, encoder=None):
     retval = ""
     if encoder is None:
         encoder = TomlEncoder(o.__class__)
-    addtoretval, sections = encoder._dump_sections(o, "")
+    addtoretval, sections = encoder.dump_sections(o, "")
     retval += addtoretval
     while sections:
         newsections = encoder.get_empty_table()
         for section in sections:
-            addtoretval, addtosections = encoder._dump_sections(
+            addtoretval, addtosections = encoder.dump_sections(
                 sections[section], section)
 
             if addtoretval or (not addtoretval and not addtosections):
@@ -62,104 +62,6 @@ def dumps(o, encoder=None):
                 newsections[section + "." + s] = addtosections[s]
         sections = newsections
     return retval
-
-
-class TomlEncoder(object):
-
-    def __init__(self, _dict=dict, preserve=False):
-        self._dict = _dict
-        self.preserve = preserve
-
-    def get_empty_table(self):
-        return self._dict()
-
-    def _dump_sections(self, o, sup):
-        retstr = ""
-        if sup != "" and sup[-1] != ".":
-            sup += '.'
-        retdict = self._dict()
-        arraystr = ""
-        for section in o:
-            section = unicode(section)
-            qsection = section
-            if not re.match(r'^[A-Za-z0-9_-]+$', section):
-                if '"' in section:
-                    qsection = "'" + section + "'"
-                else:
-                    qsection = '"' + section + '"'
-            if not isinstance(o[section], dict):
-                arrayoftables = False
-                if isinstance(o[section], list):
-                    for a in o[section]:
-                        if isinstance(a, dict):
-                            arrayoftables = True
-                if arrayoftables:
-                    for a in o[section]:
-                        arraytabstr = "\n"
-                        arraystr += "[[" + sup + qsection + "]]\n"
-                        s, d = self._dump_sections(a, sup + qsection)
-                        if s:
-                            if s[0] == "[":
-                                arraytabstr += s
-                            else:
-                                arraystr += s
-                        while d:
-                            newd = self._dict()
-                            for dsec in d:
-                                s1, d1 = self._dump_sections(d[dsec], sup +
-                                                             qsection + "." +
-                                                             dsec)
-                                if s1:
-                                    arraytabstr += ("[" + sup + qsection +
-                                                    "." + dsec + "]\n")
-                                    arraytabstr += s1
-                                for s1 in d1:
-                                    newd[dsec + "." + s1] = d1[s1]
-                            d = newd
-                        arraystr += arraytabstr
-                else:
-                    if o[section] is not None:
-                        retstr += (qsection + " = " +
-                                   unicode(_dump_value(o[section])) + '\n')
-            elif self.preserve and isinstance(o[section], InlineTableDict):
-                retstr += (qsection + " = " + _dump_inline_table(o[section]))
-            else:
-                retdict[qsection] = o[section]
-        retstr += arraystr
-        return (retstr, retdict)
-
-
-def _dump_inline_table(section):
-    """Preserve inline table in its compact syntax instead of expanding
-    into subsection.
-
-    https://github.com/toml-lang/toml#user-content-inline-table
-    """
-    retval = ""
-    if isinstance(section, dict):
-        val_list = []
-        for k, v in section.items():
-            val = _dump_inline_table(v)
-            val_list.append(k + " = " + val)
-        retval += "{ " + ", ".join(val_list) + " }\n"
-        return retval
-    else:
-        return unicode(_dump_value(section))
-
-
-def _dump_value(v):
-    dump_funcs = {
-        str: lambda: _dump_str(v),
-        unicode: lambda: _dump_str(v),
-        list: lambda: _dump_list(v),
-        bool: lambda: unicode(v).lower(),
-        float: lambda: _dump_float(v),
-        datetime.datetime: lambda: v.isoformat().replace('+00:00', 'Z'),
-    }
-    # Lookup function corresponding to v's type
-    dump_fn = dump_funcs.get(type(v))
-    # Evaluate function (if it exists) else return v
-    return dump_fn() if dump_fn is not None else v
 
 
 def _dump_str(v):
@@ -190,13 +92,109 @@ def _dump_str(v):
     return unicode('"' + v[0] + '"')
 
 
-def _dump_list(v):
-    retval = "["
-    for u in v:
-        retval += " " + unicode(_dump_value(u)) + ","
-    retval += "]"
-    return retval
-
-
 def _dump_float(v):
     return "{0:.16}".format(v).replace("e+0", "e+").replace("e-0", "e-")
+
+
+class TomlEncoder(object):
+
+    def __init__(self, _dict=dict, preserve=False):
+        self._dict = _dict
+        self.preserve = preserve
+        self.dump_funcs = {
+            str: _dump_str,
+            unicode: _dump_str,
+            list: self.dump_list,
+            bool: lambda v: unicode(v).lower(),
+            float: _dump_float,
+            datetime.datetime: lambda v: v.isoformat().replace('+00:00', 'Z'),
+        }
+
+    def get_empty_table(self):
+        return self._dict()
+
+    def dump_list(self, v):
+        retval = "["
+        for u in v:
+            retval += " " + unicode(self.dump_value(u)) + ","
+        retval += "]"
+        return retval
+
+    def dump_inline_table(self, section):
+        """Preserve inline table in its compact syntax instead of expanding
+        into subsection.
+
+        https://github.com/toml-lang/toml#user-content-inline-table
+        """
+        retval = ""
+        if isinstance(section, dict):
+            val_list = []
+            for k, v in section.items():
+                val = self.dump_inline_table(v)
+                val_list.append(k + " = " + val)
+            retval += "{ " + ", ".join(val_list) + " }\n"
+            return retval
+        else:
+            return unicode(self.dump_value(section))
+
+    def dump_value(self, v):
+        # Lookup function corresponding to v's type
+        dump_fn = self.dump_funcs.get(type(v))
+        # Evaluate function (if it exists) else return v
+        return dump_fn(v) if dump_fn is not None else v
+
+    def dump_sections(self, o, sup):
+        retstr = ""
+        if sup != "" and sup[-1] != ".":
+            sup += '.'
+        retdict = self._dict()
+        arraystr = ""
+        for section in o:
+            section = unicode(section)
+            qsection = section
+            if not re.match(r'^[A-Za-z0-9_-]+$', section):
+                if '"' in section:
+                    qsection = "'" + section + "'"
+                else:
+                    qsection = '"' + section + '"'
+            if not isinstance(o[section], dict):
+                arrayoftables = False
+                if isinstance(o[section], list):
+                    for a in o[section]:
+                        if isinstance(a, dict):
+                            arrayoftables = True
+                if arrayoftables:
+                    for a in o[section]:
+                        arraytabstr = "\n"
+                        arraystr += "[[" + sup + qsection + "]]\n"
+                        s, d = self.dump_sections(a, sup + qsection)
+                        if s:
+                            if s[0] == "[":
+                                arraytabstr += s
+                            else:
+                                arraystr += s
+                        while d:
+                            newd = self._dict()
+                            for dsec in d:
+                                s1, d1 = self.dump_sections(d[dsec], sup +
+                                                            qsection + "." +
+                                                            dsec)
+                                if s1:
+                                    arraytabstr += ("[" + sup + qsection +
+                                                    "." + dsec + "]\n")
+                                    arraytabstr += s1
+                                for s1 in d1:
+                                    newd[dsec + "." + s1] = d1[s1]
+                            d = newd
+                        arraystr += arraytabstr
+                else:
+                    if o[section] is not None:
+                        retstr += (qsection + " = " +
+                                   unicode(self.dump_value(o[section])) + '\n')
+            elif self.preserve and isinstance(o[section], InlineTableDict):
+                retstr += (qsection + " = " +
+                           self.dump_inline_table(o[section]))
+            else:
+                retdict[qsection] = o[section]
+        retstr += arraystr
+        return (retstr, retdict)
