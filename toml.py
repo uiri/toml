caed30e88d5aa822648610584b5c4ba0f79591e6
Uiri <uiri00@gmail.com>
2018-06-30 02:12:11 -0400

    Fix #167: Backslash detection bug
    Add backslash detection inside strings inside inline tables inside
    arrays of tables.

--- toml.py
+++ toml.py
@@ -707,6 +707,12 @@ def _load_array(a, _dict):
             in_str = False
             while end_group_index < len(a[1:]):
                 if a[end_group_index] == '"' or a[end_group_index] == "'":
+                    if in_str:
+                        backslash_index = end_group_index - 1
+                        while (backslash_index > -1 and
+                               a[backslash_index] == '\\'):
+                            in_str = not in_str
+                            backslash_index -= 1
                     in_str = not in_str
                 if in_str or a[end_group_index] != '}':
                     end_group_index += 1
