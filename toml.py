cc47c9b6b3e97a828bbf6b4ab5684e04e2ea6b8d
Uiri <uiri00@gmail.com>
2018-07-01 14:17:21 -0400

    Fix #160: Wrap barewords in strings

--- toml.py
+++ toml.py
@@ -942,7 +942,8 @@ def _dump_str(v):
     if v[0] == 'u':
         v = v[1:]
     singlequote = v.startswith("'")
-    v = v[1:-1]
+    if singlequote or v.startswith('"'):
+        v = v[1:-1]
     if singlequote:
         v = v.replace("\\'", "'")
         v = v.replace('"', '\\"')
