97c4be1c829f0ceab3afa1d40adf181d821fe5ec
Jon Dufresne <jon.dufresne@gmail.com>
2018-09-09 16:24:30 -0700

    Add some final trove classifiers to help document the project
    Development Status :: 5 - Production/Stable
    Intended Audience :: Developers
    Operating System :: OS Independent

--- setup.py
+++ setup.py
@@ -19,7 +19,10 @@ setup(
     license="MIT",
     long_description=readme_string,
     classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
