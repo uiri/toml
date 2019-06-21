dd1c028009f913be1da41c2ac8b365de871afa29
Filippo Broggini <filippo82@users.noreply.github.com>
2019-06-21 02:17:28 +0100

    Extend TomlNumpyEncoder to numpy integer types. (#250)
    * Extend TomlNumpyEncoder to numpy integer types with a _dump_int
    function inside the TomlNumpyEncode class.

--- toml/encoder.py
+++ toml/encoder.py
@@ -275,3 +275,9 @@ class TomlNumpyEncoder(TomlEncoder):
         self.dump_funcs[np.float16] = _dump_float
         self.dump_funcs[np.float32] = _dump_float
         self.dump_funcs[np.float64] = _dump_float
+        self.dump_funcs[np.int16] = self._dump_int
+        self.dump_funcs[np.int32] = self._dump_int
+        self.dump_funcs[np.int64] = self._dump_int
+
+    def _dump_int(self, v):
+        return "{}".format(int(v))
